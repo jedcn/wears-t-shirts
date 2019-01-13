33b14ca22e789eabe334c667c56d7518ef0e3d58
Jed Northridge <git@jedcn.com>
2019-01-13 13:05:06 -0500

    Add explanation

--- build-readme.rb
+++ build-readme.rb
@@ -9,6 +9,16 @@ require 'erb'
 template = <<-EOF
 # wears-t-shirts
 
+## What even is this?
+
+I wear one of these t-shirts each working day. I think of them as similar to
+stickers on a laptop.
+
+They are commonly under a collared shirt, but I will make sure the one I am
+wearing is visible if I am speaking publicly or interviewing someone.
+
+## Which t-shirts?
+
 <% tshirts.each do |tshirt| -%>
 * <%= tshirt["name"] %>
 <% end -%>
